465ad0d2b7ed719929e9cbbfb63421e8b8032d09
Fredrick Oluoch <menty44@gmail.com>
2021-10-26 18:26:32 +0300

    cors integration

--- lib/bestnowelixirmysql_web/router.ex
+++ lib/bestnowelixirmysql_web/router.ex
@@ -8,7 +8,6 @@ defmodule BestnowelixirmysqlWeb.Router do
     plug :put_root_layout, {BestnowelixirmysqlWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug CORSPlug
   end
 
   pipeline :api do
